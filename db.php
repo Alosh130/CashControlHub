c1c9dc00da02841da721cc360063ac56bef397e1
Alosh130 <aligamerhd15@gmail.com>
2023-12-15 15:57:44 +0300

    0.21

---
+++ db.php
@@ -0,0 +1,56 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'cashcontrolhub';
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die('Connection failed: ' . $conn->connect_error);
+    }
+    $firstName = $lastName = $userpassword = $Email = $Age = $phone = $gender = "";
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $firstName = $_POST['firstname'];
+    $lastName = $_POST['lastname'];
+    $userpassword = $_POST['password'];
+    $Email = $_POST['Email'];
+    $Age = $_POST['Birthday'];
+    $phone = $_POST['phone'];
+    $gender = $_POST['gender'];
+    $rPassword = $_POST['rpassword'];
+    }
+    $checkemail = "SELECT * FROM reg WHERE Email = '$Email'";
+    $checkresult = $conn->query($checkemail);
+
+    $checkphone = "SELECT * FROM reg WHERE Phone = '$phone'";
+    $checkPresult = $conn->query($checkphone);
+
+    if($checkresult->num_rows > 0 || $checkPresult->num_rows >0){
+        //Email and phone already exists
+        echo"Email or Phone already exists!";
+    }else{
+        if($userpassword != $rPassword){
+           echo '<script>alert("Passwords doesn\'t Match")</script>';
+        }else{
+            $sql = "INSERT INTO `cashcontrolhub`.`reg` (FirstName, LastName, Password,Email, Birthday,Phone, Gender)
+            VALUES ('$firstName', '$lastName', '$userpassword','$Email', '$Age', '$phone', '$gender')";
+
+            if($conn->query($sql)=== TRUE){
+                echo $firstName . ' ' . $lastName . ' Successfully Registered!';
+            }else{
+                echo 'Error: '. $conn->error;
+            }
+        }
+        
+    }
+    // Close connection
+    $conn->close();
+} else {
+    http_response_code(405); // Method Not Allowed
+    echo 'Method Not Allowed';
+}
+?>
